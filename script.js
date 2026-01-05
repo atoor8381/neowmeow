0f4c0daf512c96802a01c1e994ff901ce92f9253
atoor0807 <atoor0807@gmail.com>
2026-01-06 03:53:52 +0500

    add the milliseconds and change the style of the days boxes

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const endOfYear = new Date(year + 1, 0, 1);
 const MS_PER_DAY = 1000 * 60 * 60 * 24;
 const totalDays = Math.round((endOfYear - startOfYear) / MS_PER_DAY);
 
-// Create boxes
 for (let i = 0; i < totalDays; i++) {
   const day = document.createElement("div");
   day.classList.add("day");
@@ -22,24 +21,24 @@ function update() {
 
   const daysPassed = Math.floor((now - startOfYear) / MS_PER_DAY);
 
-  // Paint completed days
   document.querySelectorAll(".day").forEach((box, index) => {
     box.classList.toggle("completed", index < daysPassed);
   });
 
-  // Remaining time
   const diff = endOfYear - now;
 
   const days = Math.floor(diff / MS_PER_DAY);
   const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((diff / (1000 * 60)) % 60);
   const seconds = Math.floor((diff / 1000) % 60);
+  const milliseconds = Math.floor(diff % 1000);
 
   remainingTime.textContent = 
     `${days} days ${hours.toString().padStart(2,"0")}:` +
     `${minutes.toString().padStart(2,"0")}:` +
-    `${seconds.toString().padStart(2,"0")}`;
+    `${seconds.toString().padStart(2,"0")}: ` +
+    `${milliseconds.toString().padStart(3,"0")}`;
 }
 
 update();
-setInterval(update, 1000);
+setInterval(update, 1);
